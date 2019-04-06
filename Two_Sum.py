2878bee38e12e722af659ea9d5b8eee5abdfe0c7
Bhao <haobotao000@gmail.com>
2019-04-06 15:19:02 -0400

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,6 +1,4 @@
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-
-#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# Problem: Given an array of integers, return indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
 # Solution 1: Brute force
 def twoSum(nums, target):
@@ -13,5 +11,24 @@ def twoSum(nums, target):
 # Space Compexity: O(1)
 
 # Solution 2: Two pass Hash Table
+def twoSum(nums, target):
+    hash_table = {}
+    for i in range(len(nums)):
+        hash_table[nums[i]] = i
+    for i in range(len(nums)):
+        if target - nums[i] in hash_table:
+            if hash_table[target - nums[i]] != i:
+                return i, hash_table[target - nums[i]]
+                
+
+# Solution 3: One pass Hash Table
+def twoSum(nums, target):
+    hash_table = {}
+    for i, num in enumerate(nums, start = 0):
+        if target - num in hash_table:
+            return hash_table[target - num], i
+        hash_table[num] = i
+        
+
 
         
