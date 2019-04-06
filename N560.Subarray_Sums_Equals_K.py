a0c316503228354f57be2922cf028b8b3ef61974
Bhao <haobotao000@gmail.com>
2019-04-06 16:54:26 -0400

    update

---
+++ N560.Subarray_Sums_Equals_K.py
@@ -0,0 +1,27 @@
+# Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+# Solution 1: Brute Force
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if sum(nums[i:(j+1)]) == k:
+                    count += 1
+        return count
+        
+# Time complexity: O(n^3); Space complexity: O(1)
+
+# Solution 2: Brute Force with Prefix Sum
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        for i in range(len(nums)):
+            prefix_sum = 0
+            for j in range(i, len(nums)):
+                prefix_sum += nums[j]
+                if prefix_sum == k:
+                    count += 1
+        return count 
+            
+    
